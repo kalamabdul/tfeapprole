78a79e28102ff72e7e2a75870d3e553a4b0ab3e1
kalamabdul <83237170+kalamabdul@users.noreply.github.com>
2025-04-05 11:52:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,22 +64,6 @@ resource "vault_identity_entity_alias" "test" {
   canonical_id    = vault_identity_entity.entity[each.key].id
 }
 
-# KV Read/Write rule
-data "vault_policy_document" "kv_rw_policy" {
-  rule {
-    path         = "${var.kv_mount_path}/data/ait-73837/{{identity.entity.name}}/token"
-    capabilities = ["create", "read", "update", "delete", "list"]
-    description  = "allow KV V2 Read Write on secrets"
-  }
-}
-
-
-resource "vault_policy" "kv_rw_policy" {
-  name = "kv_rw_policy"
-  policy = data.vault_policy_document.kv_rw_policy.hcl
-}
-
-
 resource "vault_approle_auth_backend_role" "entity-role" {
   backend        = "approle"
   for_each = toset(var.entities)
